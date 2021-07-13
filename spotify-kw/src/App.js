c6bd51ad7418d0085ba88ba599ff6f009d42d0d7
salsabilaathika-97 <salsabilaathika@student.ub.ac.id>
2021-07-13 17:35:13 +0800

    modify app.js and add data.js

--- spotify-kw/src/App.js
+++ spotify-kw/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create Playlist</h1>
+        <img src = "https://i.scdn.co/image/ab67616d0000b2733611f1a3f4dceacc1ca54010"/>
+        <h3>Title</h3>
+        <h3>Artists</h3>
+        <h3>Albums</h3>
+        <button>Select</button>
     </div>
   );
 }
