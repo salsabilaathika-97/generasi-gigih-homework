b6ea688840f853370b946a0dd5efd29f38dfbaee
salsabilaathika-97 <salsabilaathika@student.ub.ac.id>
2021-07-17 21:23:08 +0800

    make component for track list

--- spotify-kw/src/App.js
+++ spotify-kw/src/App.js
@@ -1,21 +1,30 @@
-import './App.css';
-import data from './Data.js';
-import { Album, Artist, Title, Image } from './Components';
-
+import "./App.css";
+import data from "./Data.js";
+import { Album, Artist, Title, Image, RowTable } from "./Components";
 
 function App() {
   return (
     <div className="App">
       <h1>Create Playlist</h1>
-        {/* <img src = {data.album.images[0].url} alt = "Queen"/> */}
-        <Image url = {data.album.images[0].url} alt = "Queen"/>
-        {/* <h3 id = "title">{data.album.name}</h3> */}
-        <Title name = {data.album.name}/>
-        {/* <h3 id = "artist">{data.album.artists[0].name}</h3> */}
-        <Artist name = {data.album.artists[0].name} />
-        {/* <h3 id = "album">{data.name}</h3> */}
-        <Album name = {data.name}/>
-        <button>Select</button>
+      <table id="t01">
+        <tr>
+          <th>Gambar Album</th>
+          <th>Nama Artis</th>
+          <th>Nama Album</th>
+          <th>Tanggal Rilis</th>
+          <th>Total Track</th>
+        </tr>
+        {data.map((element) => (
+          <RowTable
+            url={element.album.images[2].url}
+            artist_name={element.artists[0].name}
+            album_name={element.album.name}
+            release_date={element.album.release_date}
+            total_tracks={element.album.total_tracks}
+          />
+        ))}
+      </table>
+      <button>Select</button>
     </div>
   );
 }
--- spotify-kw/src/Components/Spotify/index.js
+++ spotify-kw/src/Components/Spotify/index.js
@@ -2,5 +2,6 @@ import Album from "./Album";
 import Artist from "./Artist";
 import Title from "./Title";
 import Image from "./Image";
+import RowTable from "./RowTable";
 
-export {Album,Artist,Title,Image};
+export {Album,Artist,Title,Image,RowTable};
