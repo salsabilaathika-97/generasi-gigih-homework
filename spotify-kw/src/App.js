1c7f61b336116dd2394f1d59799e305de6408883
salsabilaathika-97 <salsabilaathika@student.ub.ac.id>
2021-07-20 22:28:36 +0800

    add login spotify

--- spotify-kw/src/App.js
+++ spotify-kw/src/App.js
@@ -1,32 +1,20 @@
-import "./App.css";
-import data from "./Data.js";
-import { Album, Artist, Title, Image, RowTable } from "./Components";
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Create Playlist</h1>
-      <table id="t01">
-        <tr>
-          <th>Gambar Album</th>
-          <th>Nama Artis</th>
-          <th>Nama Album</th>
-          <th>Tanggal Rilis</th>
-          <th>Total Track</th>
-        </tr>
-        {data.map((element) => (
-          <RowTable
-            url={element.album.images[2].url}
-            artist_name={element.artists[0].name}
-            album_name={element.album.name}
-            release_date={element.album.release_date}
-            total_tracks={element.album.total_tracks}
-          />
-        ))}
-      </table>
-      <button>Select</button>
-    </div>
-  );
+export default class App extends Component {
+    handleClick = async () => {
+        let client_id = '2bba7390150c42c9bad5b2fe1fe7a3d7';
+        let scopes = `playlist-modify-private`;
+        let redirect_url = `http://localhost:3000`;
+        let spotifyUrl = `https:/accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_url)}`;
+        window.location = spotifyUrl;
+    }
+
+    render(){
+        return <div style={{width:'100%'}}>
+            <h1 style={{textAlign:'center'}}>Spotify Login</h1>
+            <center>
+                <button onClick={this.handleClick} style={{fontSize:'20px'}}>Login</button>
+            </center>
+        </div>
+    }
 }
-
-export default App;
